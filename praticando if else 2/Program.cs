14d89b6e7705d1992ab263daabcf29f9a444ce63
Larieli Vitoria <larielivitoria@gmail.com>
2025-04-07 18:12:33 -0300

    Adicionar arquivos de projeto.

---
+++ praticando if else 2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Qual o momento do dia? manhã tarde ou noite?");
+string momentoDoDia = Console.ReadLine();
+
+Console.WriteLine("Digite seu nome:");
+string nome =  Console.ReadLine();
+
+switch (momentoDoDia)
+{
+    case "manhã":
+        Console.WriteLine($"Bom dia, seja muito bem vinda {nome}");
+        break;
+    case "tarde":
+        Console.WriteLine($"Boa tarde, seja muito bem vinda {nome}");
+        break;
+    case "noite":
+        Console.WriteLine($"Boa noite, seja muito bem vinda {nome}");
+        break;
+    default:
+        Console.WriteLine("Opção inválida");
+        break;
+}
